73f208b84859d3e5d45cecc04232f387ed4c6160
khushboo kumari <ksingh200601@gmail.com>
2025-07-18 10:56:14 +0530

    installed vite and tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+     <div className="text-3xl font-bold text-blue-600 p-4">
+      Hello Tailwind + TypeScript + Vite! 
+    </div>
+    <div className='text-xl font-base text-green-300'>
+      hii
+    </div>
+    </div>
+  
+  )
+}
+
+export default App
